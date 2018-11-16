5e9f575f1236daf3a827b4f10d1af4dd6d5b1b7a
Dana Seidel <dpseidel@berkeley.edu>
2018-11-16 13:02:11 -0800

    Add getSim function

--- R/GetSimultaneous.R
+++ R/GetSimultaneous.R
@@ -96,4 +96,130 @@ GetSimultaneous <- function(traj1,traj2,tc=0){
   #Return the two ltraj objects
   return(c(out.traj1,out.traj2))
 }
-#=============== End of GetSimultaneous Function ===============================
+#=============== End of GetSimultaneous Function ================================
+
+#' getSim: A faster GetSimultaneous function
+#' 
+#' @param df1 a data frame which contains the time-stamped
+#'    movement fixes of the first object. This function expects your dataframe to
+#'    have columns `date`, `x`, and `y` - in the format of a ltraj data frame.  
+#' @param df2 a data frame which contains the time-stamped
+#'    movement fixes of the first object. 
+#' @param tc time threshold for determining simultaneous fixes. For simplicity, \code{tc}
+#'    is always taken in seconds. If NULL, this adapted function will calculate tolerane on the fly
+#'    between 0 and 600 seconds. Intended for use with 15 min, 20 min, and 30 min, regular trajectories.
+#' @return a list of two dataframe, each subset to the simultaneous timesteps shared between the dyad.  
+#' @export
+
+getSim <- function(df1, df2, tc = NULL) {
+  
+  if (!requireNamespace("lubridate", quietly = TRUE)) {
+    stop("The lubridate package must be installed for this function to work. Please install it.",
+         call. = FALSE
+    )
+  }
+  
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop("The dplyr package must be installed for this function to work. Please install it.",
+         call. = FALSE
+    )
+  }
+  
+  # calculate tolerance on the fly for our clean trajectories.
+  # this should ensure that we don't have to do further indexing.
+  if (is.null(tc)) {
+    # median because there irregularities, especially early
+    fixes <- sort(c(median(df1$dt, na.rm = T), median(df2$dt, na.rm = T)))
+    tc <- case_when(
+      fixes[1] == fixes[2] ~ 0,
+      identical(fixes, c(900, 1800)) ~ 0,
+      identical(fixes, c(900, 1200)) ~ 300,
+      identical(fixes, c(1200, 1800)) ~ 600
+    )
+  }
+  
+  # subset to only those sharing the same dates
+  # should (in some cases) drastically reduce the datasets and thus memory issues
+  matrix(c(range(df1$date), range(df2$date)), 2, 2)
+  as.POSIXlt(apply(matrix(c(range(df1$date), range(df2$date)), 2, 2), 1, min),
+             origin = "1970-01-01 00:00:00", tz = "UTC"
+  )
+  
+  int1 <- lubridate::interval(start = min(df1$date), end = max(df1$date))
+  int2 <- lubridate::interval(start = min(df2$date), end = max(df2$date))
+  
+  if (lubridate::int_overlaps(int1, int2) == FALSE) {
+    return(NULL)
+  }
+  
+  dates <- lubridate::intersect(int1, int2)
+  tr1 <- df1[df1$date %within% dates, ]
+  tr2 <- df2[df2$date %within% dates, ]
+  
+  n1 <- dim(tr1)[1]
+  n2 <- dim(tr2)[1]
+  
+  min_diff <- function(t1, t2) {
+    which.min(abs(difftime(t1, t2, units = "secs")))
+  }
+  
+  id1 <- 1:n1
+  diff1 <- sapply(tr1$date, min_diff, t2 = tr2$date) # this is the heavyweight
+  dt1 <- mapply(tr1$date, diff1,
+                FUN = function(tr, match) {
+                  abs(difftime(tr, tr2$date[match], units = "secs"))
+                }
+  )
+  
+  id2 <- 1:n2
+  diff2 <- sapply(tr2$date, min_diff, t2 = tr1$date) # this is the heavyweight
+  dt2 <- mapply(tr2$date, diff2,
+                FUN = function(tr, match) {
+                  abs(difftime(tr, tr1$date[match], units = "secs"))
+                }
+  )
+  
+  # early returns to speed things up
+  if (tc == 0) {
+    return(list(tr1[which(dt1 <= tc), ], tr2[which(dt2 <= tc), ]))
+  }
+  
+  match1 <- tibble(tr1 = id1, tr2 = diff1, dt = dt1)[which(dt1 <= tc), ]
+  match2 <- tibble(tr1 = diff2, tr2 = id2, dt = dt2)[which(dt2 <= tc), ]
+  
+  if (nrow(match1) == nrow(match2)) {
+    return(list(tr1[which(dt1 <= tc), ], tr2[which(dt2 <= tc), ]))
+  }
+  
+  # the following still require more copying than i would like...
+  # should vectorize some of this
+  # thankfully it doesn't add significantly to memory requirements
+  
+  ind1 <- sapply(unique(match1)$tr2, function(x) {
+    ind <- which(match1$tr2 == x)
+    ind[which.min(match1$dt[ind])]
+  })
+  match1 <- match1[unique(ind1), ]
+  
+  ind2 <- sapply(unique(match1)$tr2, function(x) {
+    ind <- which(match1$tr2 == x)
+    ind[which.min(match1$dt[ind])]
+  })
+  match2 <- match2[unique(ind2), ]
+  
+  ind.1 <- which(is.na(match(match1$tr1, match2$tr1)) == TRUE)
+  if (length(ind.1) > 0) {
+    match1 <- match1[-ind.1, ]
+  }
+  ind.2 <- which(is.na(match(match2$tr1, match1$tr1)) == TRUE)
+  if (length(ind.1) > 0) {
+    match2 <- match2[-ind.2, ]
+  }
+  
+  tr1.sim <- tr1[match1$tr1, c(1:3, 11)] # dropping ltraj cols as they could be wrong for new subset.
+  tr2.sim <- tr2[match1$tr2, c(1:3, 11)]
+  
+  
+  return(list(tr1.sim, tr2.sim))
+}
+
